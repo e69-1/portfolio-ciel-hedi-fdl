5d93d5eb1d57d9ecbd9b4bc8e863c15cd1c8d4fc
e69-1 <hubg521@gmail.com>
2025-11-07 14:35:49 +0000

    feat: Ajout de script.js et première interaction DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Script chargé !"); 
+const titrePrincipal = document.querySelector('#titre');
+
+
+titrePrincipal.addEventListener('click', () => {
+  // Ce code ne s'exécute QUE si l'utilisateur clique.
+  console.log('Bouton cliqué !');
+});
+
+titrePrincipal.Vous avez cliqué sur le titre ! = "Nouveau Titre !";
